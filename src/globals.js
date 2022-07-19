fbdbe5651247fe23cbcbc7e4167a6ad8bb8b9c28
Parker Bullington-McGuire <pbullingtonmcguire@gmail.com>
2022-07-19 16:44:06 -0400

    fixed globals.js and imported variables from it

--- src/globals.js
+++ src/globals.js
@@ -1,3 +1,4 @@
 import REACT_APP_API_KEY from './.env'
-export default ASTRONOMY_PICTURES_API_KEY
+const API_KEY = REACT_APP_API_KEY
+export default API_KEY
 export const BASE_URL = 'https://api.nasa.gov/planetary/apod?'
