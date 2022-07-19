48a6999e091c223be6c4eaa3284ee992dbfdc546
Parker Bullington-McGuire <pbullingtonmcguire@gmail.com>
2022-07-19 15:55:22 -0400

    added an example image

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ function App() {
   return (
     <div className="App">
       <h1>Astronomy Picture of the Day</h1>
+      <img 
+        class='daily-picture' 
+        src='https://apod.nasa.gov/apod/image/2207/HalfPleiades_Venkatraman_1365.jpg' 
+        alt=''
+      />
     </div>
   );
 }
