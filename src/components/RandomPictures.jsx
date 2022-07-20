60fe917915327f5c1f5716ff6a8155bc0fa3f5da
Parker Bullington-McGuire <pbullingtonmcguire@gmail.com>
2022-07-20 16:59:39 -0400

    made RandomPictures get 5 random astronomy pics and log them to the console on load

--- src/components/RandomPictures.jsx
+++ src/components/RandomPictures.jsx
@@ -1,4 +1,22 @@
+import axios from "axios"
+import { useState, useEffect } from "react"
+
+import { BASE_URL } from "../globals"
+
 const RandomPictures = () => {
+
+    const [pictures, setPictures] = useState([])
+
+    const getPictures = async () => {
+        const res = await axios.get(BASE_URL + '&count=5')
+        const arr = res.data
+        // console.log(arr)
+        setPictures(arr)
+    }
+
+    useEffect(() => {
+        getPictures()
+    }, [])
 
     return (
         <div></div>
